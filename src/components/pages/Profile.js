1760bf0f324f2455606aeb865a8f08f4ad180ef5
arafatakhter505 <arafatakhterforhad@gmail.com>
2022-10-25 14:15:51 +0600

    profile page update

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,18 +1,35 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../contexts/UserContext";
 import { toast } from "react-hot-toast";
+import { profile } from "../../assets";
 
 const Profile = () => {
-  const { logOut } = useContext(AuthContext);
+  const { user, logOut } = useContext(AuthContext);
   const handleLogOut = () => {
     logOut()
       .then(() => toast.success("Log Out Successfully"))
       .catch((e) => toast.error(e.message));
   };
+
   return (
-    <div>
-      <h2>This is profile page</h2>
-      <button onClick={handleLogOut}>Log Out</button>
+    <div className="flex justify-center mt-12">
+      <div className="w-80 p-6 shadow-2xl rounded flex flex-col items-center">
+        <img
+          src={user?.photoURL ? user?.photoURL : profile}
+          className="rounded-full w-[100px] mb-8 border-2 border-sky-500"
+          alt="profile"
+        />
+        <h3 className="text-sky-500 text-2xl font-semibold">
+          {user?.displayName}
+        </h3>
+        <p>Email: {user?.email}</p>
+        <button
+          onClick={handleLogOut}
+          className="bg-sky-500 text-white w-full px-4 py-2 mt-8"
+        >
+          Log Out
+        </button>
+      </div>
     </div>
   );
 };
